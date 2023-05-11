f84f05af42ddfbe01d723c2a7f94bbd68a99d66f
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-05-11 18:12:00 -0400

    random changes

---
+++ products.js
@@ -0,0 +1,67 @@
+const product = [
+
+    {
+    id: 0,
+    name: "Mixed salad",
+    price: 19.99, 
+    description: "Organic tomatoe, lettuce and cucumber.",
+    imgSrc: "./assets/product1.jpeg",
+    },
+
+    {
+    id: 1,
+    name: "Lime",
+    price: 1.99,
+    description: "California organic green limes.",
+    imgSrc: "./assets/product2.jpeg",
+    },
+
+    {
+    id: 2,
+    name: "Green eggplant",
+    price: 5.99,
+    description: "Locally-grown organic green eggplant.",
+    imgSrc: "./assets/product3.jpeg",
+    },
+
+    {
+    id: 3,
+    name: "Zucchini",
+    price: 3.99,
+    description: "Locally-grown organic zucchini.",
+    imgSrc: "./assets/product4.jpeg",
+    },
+
+    {
+    id: 4,
+    name: "Snap peas",
+    price: 6.99,
+    description: "Locally-grown organic snap peas.",
+    imgSrc: "./assets/product5.jpeg",
+    },
+
+    {
+    id: 5,
+    name: "Green olives",
+    price: 8.99,
+    description: "Organic castelvetrano olives.",
+    imgSrc: "./assets/product6.jpeg",
+    },
+
+    {
+    id: 6,
+    name: "Green cabbage",
+    price: 6.99,
+    description: "Locally-grown organic cabbage.",
+    imgSrc: "./assets/product7.jpeg",
+    },
+
+    {
+    id: 7,
+    name: "Boston lettuce",
+    price: 7.99,
+    description: "Hydroponic organic Boston lettuce.",
+    imgSrc: "./assets/product8.jpeg",
+    }
+
+]
