66ea1527d25e6b97060446120411d4a4c80b296b
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-06-12 00:18:37 -0400

    updated trash icon functionality

--- app.js
+++ app.js
@@ -1,90 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, get, update } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
 
-  import { getDatabase, ref, get, update, onValue } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3ns1VQvYSGOMuoyxUTUSWGNT5iR2TNzo",
+  authDomain: "practice-project-app-608de.firebaseapp.com",
+  projectId: "practice-project-app-608de",
+  storageBucket: "practice-project-app-608de.appspot.com",
+  messagingSenderId: "126461480563",
+  appId: "1:126461480563:web:ebdcf28a19991bf790e406"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyD3ns1VQvYSGOMuoyxUTUSWGNT5iR2TNzo",
-    authDomain: "practice-project-app-608de.firebaseapp.com",
-    projectId: "practice-project-app-608de",
-    storageBucket: "practice-project-app-608de.appspot.com",
-    messagingSenderId: "126461480563",
-    appId: "1:126461480563:web:ebdcf28a19991bf790e406"
-  };
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const dbRef = ref(database);
 
-  // Initialize Firebase
+const body = document.body;
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+body.appendChild(overlay);
 
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  const dbRef = ref(database);
-
-  const body = document.body;
-  const overlay = document.createElement('div');
-  overlay.classList.add('overlay');
-  body.appendChild(overlay);
-
-//click on cart icon to open the cart app
-    //target the cart icon and save in a variable. 
-    const cartIconEl = document.querySelector('.cartIcon');
-    //target the cart app and save it in a variable.
-    const cartAppEl = document.querySelector('.cartApp');
+const cartIconEl = document.querySelector('.cartIcon');
+const cartAppEl = document.querySelector('.cartApp');
+const closeCartEl = document.querySelector('.closeCart');
+const continueShoppingEl = document.querySelector('.continueShopping');
 
 //create a function to toggle the cart app
 
 function toggleCart() {
-    //we want code block to toggle the cart. 
     cartAppEl.classList.toggle('activated');
     overlay.classList.toggle('activated');
 }
 
-    //addEventListener to the cart icon
-    //we want to add the event listener to the element that will trigger an action. 
-    cartIconEl.addEventListener('click', function() {
-        //code block to open the cart.
-        //display cart app
-        toggleCart(); //calling the function, a function will not run if you don't call it. 
-    })
+cartIconEl.addEventListener('click', toggleCart);
 
-//click on close icon to close the cart app
-    //target the close icon and save it in a variable.
-    const closeCartEl = document.querySelector('.closeCart');
-    //addEventListener to the close icon
-    closeCartEl.addEventListener('click', function(){ //first argument is an event. in this case the event is a click. 
-        //code block to remove cart from the display. 
-        //cartAppEl.classList.toggle('activated');
-        //cartAppEl.style.display = "none"; if we use this, the cart will not reappear when the cart icon is clicked. 
-        toggleCart();
-    });
+closeCartEl.addEventListener('click', toggleCart);
 
-//click on the continue shopping link 
-    //target the continue shopping link and save it in variable. 
-    const continueShoppingEl = document.querySelector('.continueShopping');
-    //addEventlistner
-        //on click you want to run a function to close the cart app. 
-    continueShoppingEl.addEventListener('click', function() {
-        //remove the cart from the display. 
-        toggleCart();
-    });
+continueShoppingEl.addEventListener('click', toggleCart);
 
-    window.addEventListener('click', function(event) {
-        if (
-          cartAppEl.classList.contains('activated') &&
-          !cartAppEl.contains(event.target) &&
-          !cartIconEl.contains(event.target) &&
-          ![...addToCartBtnEl].some(button => button.contains(event.target))
-        ) {
-          toggleCart();
-        }
-      });
-    
-//doesn't need to be save in a variable because window is built into JS. 
-//addEventlistener
-//on click run a function to close the cart
-
+window.addEventListener('click', function(event) {
+    if (
+      cartAppEl.classList.contains('activated') &&
+      !cartAppEl.contains(event.target) &&
+      !cartIconEl.contains(event.target) &&
+      ![...addToCartBtnEl].some(button => button.contains(event.target))
+    ) {
+      toggleCart();
+    }
+  });
+  
 const addToCartBtnEl = document.querySelectorAll('.addToCartBtn');
 const emptyCartStuff = document.querySelector('.emptyCartStuff');
 const notEmptyCartStuff = document.querySelector('.notEmptyCartStuff');
@@ -202,6 +170,7 @@ function renderCartItems(cartItemsArray) {
     const trashIcon = document.createElement('p');
     trashIcon.classList.add('trashIcon');
     trashIcon.textContent = 'Remove';
+    trashIcon.id = `i${item.id}`; // Set the ID attribute with the item ID
 
     productBtnContainer.append(trashIcon);
 
@@ -210,10 +179,9 @@ function renderCartItems(cartItemsArray) {
       const id = event.target.id.slice(2);
       //console.log(item); // Extract item ID from the button's ID attribute
       const itemId = `item${id}`;
-      console.log(itemId);
       const childRef = ref(database, `items/${itemId}`);
 
-      if (item.quantity > 0) {
+      if (item.quantity > 1) {
         item.quantity--; // Decrement the quantity
         productQty.textContent = item.quantity; // Update the displayed quantity
         // Update the quantity in the database
@@ -228,9 +196,7 @@ function renderCartItems(cartItemsArray) {
      // Event listener for minus button
      plusBtn.addEventListener('click', function (event) {
       const id = event.target.id.slice(2); // Extract item ID from the button's ID attribute
-      console.log(id);
       const itemId = `item${id}`;
-      console.log(itemId);
 
       const childRef = ref(database, `items/${itemId}`);
 
@@ -240,14 +206,26 @@ function renderCartItems(cartItemsArray) {
         // Update the quantity in the database
         update(childRef, { quantity: item.quantity });
     });
+
+    trashIcon.addEventListener('click', function(event) {
+      const id = event.target.id.slice(2);
+      //console.log(item); // Extract item ID from the button's ID attribute
+      const itemId = `item${id}`;
+      
+      const childRef = ref(database, `items/${itemId}`);
+
+      productInCartContainer.remove('productInCartContainer');
+      update(childRef, { inCart: false, quantity: 0 });
+    })
   });
 }
 
 //if product qty = 0, remove it from the cart
   //currently there is a bug where inCart: true, quantity: 0, and one more click to remove the container makes it inCart: false, quantity: 0
 
-//need function remove item from cart
 //need function clear the cart
+
+
 //need function to increment total items in cart
 //need function to calculate subTotal
 
